9bf1a985b3e6f70ecf350244cc751a1e1a7deebe
Arwy Syahputra Siregar <arwysyahputrasiregar@Arwys-MacBook-Pro.local>
2021-01-15 19:34:21 +0700

    folder structuring and install navigation package

---
+++ src/components/parallax.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View } from 'react-native'
+
+const Parallax =()=>{
+    return(
+        <View>
+            <Text>
+                Parallax
+            </Text>
+        </View>
+    )
+}
+export default Parallax 
